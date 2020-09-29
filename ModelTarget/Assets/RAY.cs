88d25310aff50a1d978438044066e50c57277759
Nazmul Hasan <nazmulimm@gmail.com>
2020-09-29 19:00:36 +0600

    Final update

---
+++ ModelTarget/Assets/RAY.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class RAY : MonoBehaviour
+{
+    public GameObject can2;
+
+    private void Start()
+    {
+        VirtualButtonBehaviour vb = GetComponent<VirtualButtonBehaviour>();
+
+        vb.RegisterOnButtonPressed(hello);
+    }
+
+    void hello(VirtualButtonBehaviour v)
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                string name = hit.rigidbody.name;
+                if(name == "Image")
+                {
+                    can2.SetActive(true);
+                }
+            }
+        }
+    }
+}
